0441f78a6ba12d8b870c53fe70a485f8f4f7c683
Dovlat Yakshibayev <dovlat93@gmail.com>
2024-05-07 11:31:25 +0200

    remap smart splits

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -60,6 +60,10 @@ return {
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         ["<Leader>b"] = { desc = "Buffers" },
+        ["<Leader>b\\"] = false,
+        ["<Leader>b|"] = false,
+        ["<Leader>bh"] = { ":split<CR>", desc = "Horizontal Split" },
+        ["<Leader>bv"] = { ":vsplit<CR>", desc = "Vertical Split" },
         -- quick save
         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
 
