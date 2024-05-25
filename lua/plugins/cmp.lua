563d6708dfc180d252cade547832e76cbaa24f16
Dovlat Yakshibayev <dovlat93@gmail.com>
2024-05-25 15:41:02 +0200

    configure dadbod

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,18 +1,26 @@
 return {
   "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
+  config = function(_, opts)
     local cmp = require "cmp"
-    return require("astrocore").extend_tbl(opts, {
-      mapping = {
-        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-        ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-        ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-        ["<Tab>"] = {
-          i = cmp.config.disable,
-          c = cmp.config.disable,
-        },
-      },
+    cmp.setup(opts)
+
+    -- Set up sources for SQL file types
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "sql", "mysql", "plsql" },
+      callback = function()
+        cmp.setup.buffer {
+          sources = {
+            { name = "vim-dadbod-completion" },
+            { name = "buffer" },
+          },
+        }
+      end,
     })
+
+    opts.mapping["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select }
+    opts.mapping["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select }
+    opts.mapping["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select }
+    opts.mapping["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select }
+    opts.mapping["<Tab>"] = { i = cmp.config.disable, c = cmp.config.disable }
   end,
 }
